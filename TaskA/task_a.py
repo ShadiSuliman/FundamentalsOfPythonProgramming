beeb9d3fdd2f9d87ab3563c6a54082d00a0d50f7
ShadiSuliman <shadiisuliman14@gmail.com>
2026-01-25 23:52:39 +0200

    Task A completed

--- TaskA/task_a.py
+++ TaskA/task_a.py
@@ -24,6 +24,7 @@ Location: Meeting Room A
 Phone: 0401234567
 Email: anna.virtanen@example.com
 """
+from datetime import datetime
 
 def main():
     # Define the file name directly in the code
@@ -32,9 +33,40 @@ def main():
     # Open the file and read its contents
     with open(reservations, "r", encoding="utf-8") as f:
         reservation = f.read().strip()
+        reservation = reservation.split("|")
+        
+        reservation_number = int(reservation[0])
+        booker = reservation[1]
+        
+        date = datetime.strptime(reservation[2], "%Y-%m-%d").date()
+        finnish_date = date.strftime("%d.%m.%Y")
+        
+        time = datetime.strptime(reservation[3], "%H:%M").time()
+        finnish_time = time.strftime("%H.%M")
+        
+        hours = int(reservation[4])
+        hourly_price = float(reservation[5])
+        
+        paid = reservation[6] == "True"
+        
+        location = reservation[7]
+        phone = reservation[8]
+        email = reservation[9]
+        
+        total_price = hours * hourly_price
 
     # Print the reservation to the console
-    print(reservation)
+    print(f"Reservation number: {reservation_number}")
+    print(f"Booker: {booker}")
+    print(f"Date: {finnish_date}")
+    print(f"Start time: {finnish_time}")
+    print(f"Number of hours: {hours}")
+    print(f"Hourly price: {hourly_price:.2f} €".replace(".", ","))
+    print(f"Total price: {total_price:.2f} €".replace(".", ","))
+    print(f"Paid: {'Yes' if paid else 'No'}")
+    print(f"Location: {location}")
+    print(f"Phone: {phone}")
+    print(f"Email: {email}")
 
     # Try these
     #print(reservation.split('|'))
